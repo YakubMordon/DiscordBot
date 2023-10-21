3c84b3613ec90046109d41991923d6fb21534980
Jacob Mordon <YakubKazimiezh.Mordon.KN.2021@edu.lpnu.ua>
2023-10-21 17:34:22 +0300

    Added documentation for Bot class

--- Bot.cs
+++ Bot.cs
@@ -10,59 +10,100 @@ using TokenType = DSharpPlus.TokenType;
 
 namespace DiscordBot
 {
+    /// <summary>
+    /// Клас у якому виконується конфігурація бота
+    /// </summary>
     class Bot
     {
+        /// <summary>
+        /// Змінна, для роботою з Discord API
+        /// </summary>
         private DiscordClient _client;
+
+        /// <summary>
+        /// Змінна, для долучення команд у форматі: ?{command_name}
+        /// </summary>
         private CommandsNextExtension _commands;
+
+        /// <summary>
+        /// Змінна, для долучення слеш-команд
+        /// </summary>
         private SlashCommandsExtension _slashCommands;
 
+        /// <summary>
+        /// Метод для конфігурації та запуску бота Discord.
+        /// </summary>
+        /// <returns>Завдання, яке представляє асинхронний запуск бота.</returns>
         public async Task RunAsync()
         {
+            // Створення конфігурації бота.
             var config = new DiscordConfiguration
             {
-                Token = "Token_Here",
+                Token = "Token_Here", // Замініть "Token_Here" на реальний токен вашого бота.
                 TokenType = TokenType.Bot,
                 AutoReconnect = true,
                 Intents = DiscordIntents.AllUnprivileged | DiscordIntents.All
             };
 
-
+            // Ініціалізація клієнта Discord з використанням зазначеної конфігурації.
             _client = new DiscordClient(config);
 
+            // Включення підтримки голосових функцій.
             _client.UseVoiceNext();
 
+            // Додавання обробників подій.
             _client.Ready += OnClientReady;
             _client.GuildCreated += OnGuildCreated;
 
+            // Конфігурація командного модуля.
             var commandsConfig = new CommandsNextConfiguration
             {
-                StringPrefixes = new string[] { "?" },
-                EnableMentionPrefix = true
+                StringPrefixes = new string[] { "?" }, // Префікс для активації команд.
+                EnableMentionPrefix = true // Дозволяє активацію команд шляхом згадування бота.
             };
 
+            // Ініціалізація командного модуля та реєстрація команд.
             _commands = _client.UseCommandsNext(commandsConfig);
-            _commands.RegisterCommands<MusicCommands>();
-            _commands.RegisterCommands<GroupCommands>();
+            _commands.RegisterCommands<MusicCommands>(); // Реєстрація команд з класу MusicCommands.
+            _commands.RegisterCommands<GroupCommands>(); // Реєстрація команд з класу GroupCommands.
 
+            // Ініціалізація модуля для обробки Slash-команд (інтерактивних команд Discord).
             _slashCommands = _client.UseSlashCommands();
-            _slashCommands.RegisterCommands<MusicSL>();
-            _slashCommands.RegisterCommands<GroupSL>();
+            _slashCommands.RegisterCommands<MusicSL>(); // Реєстрація Slash-команд з класу MusicSL.
+            _slashCommands.RegisterCommands<GroupSL>(); // Реєстрація Slash-команд з класу GroupSL.
 
+            // Підключення до серверів Discord.
             await _client.ConnectAsync();
+
+            // Безкінечне очікування завершення виконання бота (завдяки Task.Delay(-1)).
             await Task.Delay(-1);
         }
 
+        /// <summary>
+        /// Обробник події викликається, коли Discord бот готовий до використання.
+        /// </summary>
+        /// <param name="sender">Екземпляр клієнта Discord, що викликав подію.</param>
+        /// <param name="e">Параметри події ReadyEventArgs, які містять інформацію про готовність бота.</param>
+        /// <returns>Завдання, яке позначає завершення обробки події.</returns>
         private Task OnClientReady(DiscordClient sender, ReadyEventArgs e)
         {
             Console.WriteLine("Bot is ready!");
             return Task.CompletedTask;
         }
 
-        private Task OnGuildCreated(DiscordClient sender, GuildCreateEventArgs e)
+        /// <summary>
+        /// Обробник події, яка викликається при долученні бота до сервера (гільдії) Discord.
+        /// </summary>
+        /// <param name="sender">Екземпляр клієнта Discord, що викликав подію.</param>
+        /// <param name="e">Параметри події GuildCreateEventArgs, які містять інформацію про новий сервер.</param>
+        /// <returns>Завдання, яке позначає завершення обробки події.</returns>
+        private async Task OnGuildCreated(DiscordClient sender, GuildCreateEventArgs e)
         {
-            Console.WriteLine($"Joined guild: {e.Guild.Name}");
-            return Task.CompletedTask;
+            Console.WriteLine($"Бот приєднався до гільдії: {e.Guild.Name}");
+
+            await Task.CompletedTask;
         }
+
 
     }
 
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿namespace DiscordBot
 {
+
+    /// <summary>
+    /// Головний клас у програмі.Клас Program
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Метод у якому розпочинаємо виконання програми та ініціалізуємо бота
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             var bot = new Bot();
